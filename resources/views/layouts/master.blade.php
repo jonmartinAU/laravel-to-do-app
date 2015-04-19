027ced72927a46ad6afb20ba8d743cd02a51fb63
jonmartin <jon.martin@mail.com>
2015-04-20 07:43:01 +1000

    Adding a master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Welcome to To-Do list</title>
+    <style>
+	@import url(//fonts.googleapis.com/css?family=Lato:700);
+      body {
+        margin:0;
+        font-family:'Lato', sans-serif;
+        text-align:center;
+        color: #999; }
+    </style>
+  </head>
+
+<body>
+  @yield('content')
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,11 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>Welcome to To-Do list</title>
-    <style>
-	@import url(//fonts.googleapis.com/css?family=Lato:700);
-      body {
-        margin:0;
-        font-family:'Lato', sans-serif;
-        text-align:center;
-        color: #999; }
-    </style>
-  </head>
+@extends('layouts.master') 
+@section('content')
 
-  <body>
   	<h1>Welcome to your to-do list</h1>
     <ul>
-      @foreach ($lists as $list)
+      @forelse ($lists as $list)
         <li>{{ $list }}</li> 
       @empty
         <li>You don't have any lists saved.</li>
       @endforelse
     </ul>
-
-  </body>
-
-</html>
