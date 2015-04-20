fd10f8b3d66eeffff5b77f0b1637aaad981000fb
jonmartin <jon.martin@mail.com>
2015-04-21 08:26:56 +1000

    Using a view partial on index page

---
+++ resources/views/partials/row.blade.php
@@ -0,0 +1,3 @@
+<tr style="padding-bottom: 5px;">
+	<td> Partial row test </td>
+</tr>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,15 @@
         <li>You don't have any lists saved.</li>
       @endforelse
     </ul>
+
+  <table class="table borderless">
+    @include('partials.row')
+  </table>
+
 @endsection
 
 @section('advertisement')
   @parent
   <p>Ad #2</p>
 @endsection
+
