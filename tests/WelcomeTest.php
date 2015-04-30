3d3a4aadc174d68e85bdad47eef82239050d0858
jonmartin <jon.martin@mail.com>
2015-05-01 09:05:22 +1000

    Test to confirm that when this link is clicked the user is taken to the About controller’s contact action

--- tests/WelcomeTest.php
+++ tests/WelcomeTest.php
@@ -6,16 +6,32 @@
 
  	public function testUserSeesWelcomeMessage() 
  	{
+	 	$client = new Client();
 
- 	$client = new Client();
+	 	$crawler = $client->request('GET', 'http://homestead.app/');
 
- 	$crawler = $client->request('GET', 'http://homestead.app/');
+	 	$this->assertEquals(200, $client->getResponse()->getStatus()); 
+	 	$this->assertCount(1,
 
- 	$this->assertEquals(200, $client->getResponse()->getStatus()); 
- 	$this->assertCount(1,
-
- 	$crawler->filter('h1:contains("Welcome to TODOParrot")'));
-
+	 	$crawler->filter('h1:contains("Welcome to TODOParrot")'));
  	}
+
+ 	public function testUserClicksContactLinkAndIsTakenToContactPage()
+ 	{
+		$client = new Client();
+
+		$crawler = $client->request('GET', 'http://homestead.app/');
+
+		$link = $crawler->selectLink('Contact Us')->link(); 
+		$this->assertEquals('http://homestead.app/about/contact',
+
+		$link->getUri());
+
+		$crawler = $client->click($link);
+
+		$this->assertCount(1,
+
+		$crawler->filter('h1:contains("Contact Us")'));
+	}
 
 }
