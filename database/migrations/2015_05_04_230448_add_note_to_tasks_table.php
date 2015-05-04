b1385ee96a5aecfed7de27c368d692d5c6cf247f
jonmartin <jon.martin@mail.com>
2015-05-05 09:08:32 +1000

    Added tasks table

---
+++ database/migrations/2015_05_04_230448_add_note_to_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNoteToTasksTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+
+	{
+
+	Schema::table('tasks', function(Blueprint $table) 
+	{
+
+	$table->string('note');
+
+	});
+
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('tasks', function(Blueprint $table) {
+	    $table->dropColumn('note');
+	});
+	}
+
+}
