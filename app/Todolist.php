fa87f74d65449a5e66748f71e7472764ff7267b7
jonmartin <jon.martin@mail.com>
2015-05-09 15:21:58 +1000

    Adding  array

--- app/Todolist.php
+++ app/Todolist.php
@@ -3,7 +3,10 @@
 use Illuminate\Database\Eloquent\Model;
 
 class Todolist extends Model {
+
 	private $rules = [
-		'name' => 'required', 'description' => 'required
+		'name' => 'required', 
+		'description' => 'required'
 	];
+
 }
